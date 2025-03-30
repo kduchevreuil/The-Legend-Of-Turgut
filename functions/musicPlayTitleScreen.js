db8da6635dcf27f7880b65248bc84511ff5e7548
kduchevreuil <150837754+kduchevreuil@users.noreply.github.com>
2025-03-30 01:51:58 +0100

    son? Oui !

--- functions/musicPlayTitleScreen.js
+++ functions/musicPlayTitleScreen.js
@@ -1,7 +1,7 @@
 function musicPlayTitleScreen() {
     const titleScreen = document.getElementById('titleScreen');
     const audio = document.createElement('audio');
-    audio.src = '../sounds/START.wav'; // Chemin vers le fichier audio 
+    audio.src = './sounds/START.wav'; // Chemin vers le fichier audio 
 
     audio.loop = false; // Mettre la musique en boucle
 
