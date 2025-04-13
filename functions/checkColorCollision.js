036c2a28a60fd1421531c0ae225921227475912f
kduchevreuil <150837754+kduchevreuil@users.noreply.github.com>
2025-04-13 22:14:35 +0200

    corrections du canvas

--- functions/checkColorCollision.js
+++ functions/checkColorCollision.js
@@ -28,36 +28,11 @@ function checkColorCollision() {
     ctx.fillStyle = "red";
     ctx.fillRect(0, 0, turgutWidth, turgutHeight);
 
-    // Capturer les données d'image
-    const imageData = ctx.getImageData(0, 0, turgutWidth, turgutHeight);
-    const data = imageData.data;
-    const colors = new Set(); // Utilisation d'un Set pour éviter les doublons
 
-    // Parcourir les pixels
-    for (let i = 0; i < data.length; i += 4) {
-        const r = data[i];
-        const g = data[i + 1];
-        const b = data[i + 2];
-        const a = data[i + 3];
 
-        // Vérifier si le pixel est opaque
-        if (a > 0) { // On accepte tous les pixels non transparents
-            const colorHex = `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
-            colors.add(colorHex.toLowerCase());
-        }
-    }
 
-    // Vérifier les collisions
-    const isProhibitedColor = [...colors].some(color => prohibitedColors.includes(color));
 
-    if (isProhibitedColor) {
-        alert("Vous êtes en contact avec une couleur interdite !");
-        // Ajouter ici le code pour gérer la collision
-        return true; // Retourne true si collision détectée
-    } else {
-        console.log("Aucune couleur interdite détectée.");
-        return false; // Retourne false si pas de collision
-    }
+
 }
 
 export default checkColorCollision;
